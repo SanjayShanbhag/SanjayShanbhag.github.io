23f1f430561a8a5a486edfe8fba48afec64b446c
Sanjay Shanbhag <sanjayb.shanbhag@gmail.com>
2018-06-22 11:58:13 +0530

    Changed some parts of clarividencia

--- Mastermind/Clairvidencia.jsx
+++ Mastermind/Clairvidencia.jsx
@@ -2,7 +2,7 @@
 <html>
   <head>
        <meta charset="UTF-8">
-       <title>First React</title>
+       <title>Clarividencia</title>
        <link rel="stylesheet" type="text/css" href="CSS/style.css">
        <script src="https://unpkg.com/react@15/dist/react.min.js"></script>
        <script src="https://unpkg.com/react-dom@15/dist/react-dom.min.js"></script>
@@ -21,16 +21,20 @@
                     color: '#FAFAFA',
                     textAlign: 'center'
                 }
+                const imgStyleObject = {
+                    width: "80",
+                    height: "40"
+                }
                 return (
                     <div className="container topLoad">
-                        <h1 style={styleObject}>{props.message}</h1>
+                        <h1 style={styleObject}><img src="images/logo.png" style={imgStyleObject}/>  {props.message}</h1>
                     </div>
                 )
             }
             class GetNumber extends React.Component{
                 constructor(props){
                     super(props);
-                    this.state = {value: '', result: '', imge: 'guess.png', status: 0, colE: '#00C853', colM: '#FAFAFA', colH: '#FAFAFA'};
+                    this.state = {value: '', result: '', imge: 'guess.png', status: 0, colE: '#00C853', colM: '#FAFAFA', colH: '#FAFAFA', chance: ''};
 
                     this.handleChange = this.handleChange.bind(this);
                     this.handleSubmit = this.handleSubmit.bind(this);
@@ -46,22 +50,27 @@
                 } 
                 playAgain(event){
                     this.randm = Math.floor(Math.random() * (10 - 1 + 1)) + 1;
-                    this.setState({value: '', result: '', imge: 'guess.png', status: 0, colE: '#00C853', colH: '#FAFAFA', colM: '#FAFAFA'})   
+                    this.chances = 0;
+                    this.setState({value: '', result: '', imge: 'guess.png', status: 0, colE: '#00C853', colH: '#FAFAFA', colM: '#FAFAFA', chance: ''})   
                 }
                 easy(){
                     this.randm = Math.floor(Math.random() * (10 - 1 + 1)) + 1;
-                    this.setState({colE: '#00C853', colH: '#FAFAFA', colM: '#FAFAFA', value: '', result: '', imge: 'guess.png', status: 0})
+                    this.chances = 0;
+                    this.setState({colE: '#00C853', colH: '#FAFAFA', colM: '#FAFAFA', value: '', result: '', imge: 'guess.png', status: 0, chance: ''})
                 }
                 medium(){
                     this.randm = Math.floor(Math.random() * (50 - 1 + 1)) + 1;
-                    this.setState({colM: '#00C853', colE: '#FAFAFA', colH: '#FAFAFA', value: '', result: '', imge: 'guess.png', status: 0})
+                    this.chances = 0;
+                    this.setState({colM: '#00C853', colE: '#FAFAFA', colH: '#FAFAFA', value: '', result: '', imge: 'guess.png', status: 0, chance: ''})
                 }
                 hard(){
                     this.randm = Math.floor(Math.random() * (100 - 1 + 1)) + 1;
-                    this.setState({colH: '#00C853', colE: '#FAFAFA', colM: '#FAFAFA', value: '', result: '', imge: 'guess.png', status: 0})
+                    this.chances = 0;
+                    this.setState({colH: '#00C853', colE: '#FAFAFA', colM: '#FAFAFA', value: '', result: '', imge: 'guess.png', status: 0, chance: ''})
                 }
                 componentDidMount(){
                     this.randm = Math.floor(Math.random() * (10 - 1 + 1)) + 1;
+                    this.chances = 0;
                 }
                 handleSubmit(event){
                     console.log(this.randm);
@@ -72,15 +81,16 @@
                         }else{
                             this.setState({result: 'Bigger', imge: 'bigger.png'});
                         }
+                        this.chances = this.chances + 1;
                     }else if(this.state.value > this.randm){
                         if((this.state.value-this.randm) <= 5){
                             this.setState({result: 'Ooh, Very Close!', imge: 'smaller.png'});    
                         }else{
                             this.setState({result: 'Smaller', imge: 'smaller.png'});
                         }
-                        
+                        this.chances = this.chances + 1;
                     }else{
-                        this.setState({result: 'You Win!!', imge: 'win.png', status: 1});
+                        this.setState({result: 'You Win!!', imge: 'win.png', status: 1, chance: this.chances+' Chances'});
                     }
                     event.preventDefault();
                 }
@@ -143,6 +153,8 @@
                         </form>
                         <br/>
                         <h3 style={stYl}>{this.state.result}</h3>
+                        <h4 style={stYl}>{this.state.chance}</h4>
+                        <br/>
                         <img src={'images/'+this.state.imge} className="center-block" width="80" height="80"/>
                         <br/>
                         {this.state.status?(<input className="btn btn-primary center-block" onClick={this.playAgain} type="submit" value="Play Again"/>):''}
@@ -150,7 +162,7 @@
                     );
                 }
             }   
-            const element = <TopName message="MasterMind Game"/>;
+            const element = <TopName message="Clarividencia"/>;
             ReactDOM.render(element, document.getElementById('topCol'));
             const element1 = <GetNumber/>
             ReactDOM.render(element1, document.getElementById('nex'));
