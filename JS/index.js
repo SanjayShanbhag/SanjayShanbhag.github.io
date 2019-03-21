cb4c952be32a80acb5df41b3617f2e088031cf4d
Sanjay Shanbhag <sanjayb.shanbhag@gmail.com>
2019-03-21 14:09:37 +0530

    New Updated Website

---
+++ JS/index.js
@@ -0,0 +1,28 @@
+function popupOpenClose(popup) {
+
+/* Open popup */
+$(popup).show();
+
+/* Close popup if user clicks on background */
+$(popup).click(function(e) {
+if ( e.target == this ) {
+if ($(popup).is(':visible')) {
+  $(popup).hide();
+}
+}
+});
+
+/* Close popup and remove errors if user clicks on cancel or close buttons */
+$(popup).find("button[name=close]").on("click", function() {
+if ($(".formElementError").is(':visible')) {
+$(".formElementError").remove();
+}
+$(popup).hide();
+});
+}
+
+$(document).ready(function () {
+$("[data-js=open]").on("click", function() {
+popupOpenClose($(".popup"));
+});
+});
